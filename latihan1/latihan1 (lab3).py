a6bacb3e375f1e0fa7cc792dd18d18718f12ba5f
febryvia_deya <febryviadeyanur@gmail.com>
2025-10-28 01:09:58 +0700

    Menambahkan file kedua saya

--- latihan1/latihan1 (lab3).py
+++ latihan1/latihan1 (lab3).py
@@ -1,14 +1,16 @@
 import random
 
-# Input nilai n pada saat runtime (dalam contoh ini, n = 5)
-n = 5  # Menggunakan nilai n yang diberikan: 5
+# Input nilai n pada saat runtime
+n = 5 #Menggunakan nilai n yang diberikan: 5
 
 # Menggunakan kombinasi while dan for
 for i in range(n):
-    while True:
-        # Menghasilkan bilangan acak antara 0 dan 1
-        r = random.random()
-        # Memastikan bilangan acak kurang dari 0.5
-        if r < 0.5:
-            print(f"Bilangan acak ke-{i+1}: {r}")
-            break
+   while True:
+    # Menghasilkan bilangan acak antara 0 dan 1
+    r = random.random()
+    # Memastikan bilangan acak kurang dari 0.5
+    if r < 0.5:
+        print(f"Bilangan acak ke-{i+1}: {r}")
+        break
+
+
